a890dc2dbf53699d022e35542a2435969ce07237
Helen Tan <helentanmz0909@gmail.com>
2022-02-18 11:32:16 +0800

    Fetch repo data + display repos in UI

--- github.js
+++ github.js
@@ -2,15 +2,22 @@ class Github {
     constructor() {
         this.client_id = '481f52e38ba22ec5e7fe';
         this.client_secret = 'e5373c8e258f7eb158c258204ece5db2938469e4';
+        this.repos_count = 5;
+        this.repos_sort = 'create: asc';
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
     
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
 
+        const repos = await repoResponse.json();
+
         return {
-            profile: profile
+            profile: profile,
+            repos: repos
         }
     }
 }
